2697dcd10a6cf8e1c12eaef5461d8d71fbf85d18
kcksteve <kcksteve@gmail.com>
2022-07-26 20:47:28 -0400

    Update RenderCampsite.js, AboutScreen.js, and 3 more files... added icons, facvorites, comments, naviagtion icons, custom side drawer

---
+++ screens/AboutScreen.js
@@ -0,0 +1,39 @@
+import { ScrollView, Text } from "react-native";
+import { PARTNERS } from "../shared/partners";
+import { useState } from "react";
+import { Avatar, Card, ListItem } from "react-native-elements";
+
+const Mission = () => {
+    return (
+        <Card>
+            <Card.Title>Our Mission</Card.Title>
+            <Card.Divider/>
+            <Text style={{margin: 10}}>We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.</Text>
+        </Card>
+    );
+}
+
+const AboutScreen = () => {
+    const [partners, setPartners] = useState(PARTNERS);
+
+    return (
+        <ScrollView>
+            <Mission/>
+            <Card>
+                <Card.Title>Community Partners</Card.Title>
+                <Card.Divider/>
+                {partners.map((item) =>
+                    <ListItem key={item.id}>
+                        <Avatar rounded source={item.image}/>
+                        <ListItem.Content>
+                            <ListItem.Title>{item.name}</ListItem.Title>
+                            <ListItem.Subtitle>{item.description}</ListItem.Subtitle>
+                        </ListItem.Content>
+                    </ListItem>
+                )}
+            </Card>
+        </ScrollView>
+    );
+}
+
+export default AboutScreen;
