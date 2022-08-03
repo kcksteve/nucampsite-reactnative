69c71b967ef17670b9924a8d8e02a500eb8341ea
kcksteve <kcksteve@gmail.com>
2022-08-02 21:53:59 -0400

    Update RenderCampsite.js, package-lock.json, and 7 more files... added animations to screens

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -3,6 +3,7 @@ import { Avatar, Card, ListItem } from "react-native-elements";
 import { useSelector } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
 import Loading from "../components/LoadingComponent";
+import * as Animatable from 'react-native-animatable'
 
 const Mission = () => {
     return (
@@ -33,32 +34,44 @@ const AboutScreen = () => {
     if (partners.errMess) {
         return (
             <ScrollView>
-                <Mission/>
-                <Card>
-                    <Card.Title>Community Partners</Card.Title>
-                    <Card.Divider/>
-                    <Text>{partners.errMess}</Text>
-                </Card>
+                <Animatable.View
+                    animation='fadeInDown'
+                    duration={2000}
+                    delay={1000}
+                >
+                    <Mission/>
+                    <Card>
+                        <Card.Title>Community Partners</Card.Title>
+                        <Card.Divider/>
+                        <Text>{partners.errMess}</Text>
+                    </Card>
+                </Animatable.View>
             </ScrollView>
         );
     }
 
     return (
         <ScrollView>
-            <Mission/>
-            <Card>
-                <Card.Title>Community Partners</Card.Title>
-                <Card.Divider/>
-                {partners.partnersArray.map((item) =>
-                    <ListItem key={item.id}>
-                        <Avatar rounded source={{ uri: baseUrl + item.image }}/>
-                        <ListItem.Content>
-                            <ListItem.Title>{item.name}</ListItem.Title>
-                            <ListItem.Subtitle>{item.description}</ListItem.Subtitle>
-                        </ListItem.Content>
-                    </ListItem>
-                )}
-            </Card>
+            <Animatable.View
+                    animation='fadeInDown'
+                    duration={2000}
+                    delay={1000}
+                >
+                <Mission/>
+                <Card>
+                    <Card.Title>Community Partners</Card.Title>
+                    <Card.Divider/>
+                    {partners.partnersArray.map((item) =>
+                        <ListItem key={item.id}>
+                            <Avatar rounded source={{ uri: baseUrl + item.image }}/>
+                            <ListItem.Content>
+                                <ListItem.Title>{item.name}</ListItem.Title>
+                                <ListItem.Subtitle>{item.description}</ListItem.Subtitle>
+                            </ListItem.Content>
+                        </ListItem>
+                    )}
+                </Card>
+            </Animatable.View>
         </ScrollView>
     );
 }
